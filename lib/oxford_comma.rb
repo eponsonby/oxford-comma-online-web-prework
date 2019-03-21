8749ddce7c95ce62014ad3b054fafd59aa29e0f6
Erin Ponsonby <erinponsonby@gmail.com>
2019-03-21 00:24:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length <= 2
     array.join(" and ")
   elsif array.length == 3
-    
-    
-    
+    new_string = array[0..1].join(" , ") + array[2].join(" and ")
+    puts new_string
+  end
 end
